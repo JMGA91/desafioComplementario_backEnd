0a957d3d863f23b1786de5eb98c42696773d212d
JMGA <jmgaleman@gmail.com>
2024-07-11 21:02:37 -0300

    Trabajando en swich roles. Se hicieron ajustes en session test y Dto.

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,5 +1,14 @@
-export const auth = (role) => (req, res, next) => {
-  if (req.user.user.role === role) return next();
+export const auth = (allowedRoles) => {
+  return (req, res, next) => {
+    if (!req.isAuthenticated()) {
+      return res.status(401).redirect("/unauthorized");
+    }
 
-  res.redirect("/unauthorized");
+    const userRole = req.user.user.role;
+    if (allowedRoles.includes(userRole)) {
+      return next();
+    }
+
+    return res.status(403).redirect("/unauthorized");
+  };
 };
